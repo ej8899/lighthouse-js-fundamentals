92fc278ee8b17f59b2e3f2b30d9e553c94a62d42
ej8899 <ej8899@gmail.com>
2022-06-03 08:31:57 -0600

    prep voting station  code -- note the FOR loop here - not used to this for array looping.

---
+++ prep-votingstations.js
@@ -0,0 +1,35 @@
+//
+// prep-votingstations
+//
+
+const stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+];
+
+console.log(chooseStations(stations));
+
+// criteria: 
+// capacity of at least 20, and be a school or community centre.
+function chooseStations(stations)
+{
+  const goodStations = [];
+  
+  for(const station of stations)
+  {
+    const capacity = station[1];
+    if( capacity >= 20)
+    {
+      // console.log(capacity);
+
+      const type = station[2];
+      if (type === 'school' || type === 'community centre')
+      {
+        // console.log(type);
+        goodStations.push(station[0]);
+      }
+    }
+  }
+  return goodStations;
+}
