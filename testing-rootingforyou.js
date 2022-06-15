783177b33a729b2b9fe548bb5ff7d9645728ca9d
ej8899 <ej8899@gmail.com>
2022-06-15 12:26:45 -0600

    misc updates 2022-06-15

---
+++ testing-rootingforyou.js
@@ -0,0 +1,32 @@
+const vegetables = [
+  {
+  submitter: 'Old Man Franklin',
+  redness: 10,
+  plumpness: 5
+  },
+  {
+  submitter: 'Sally Tomato-Grower',
+  redness: 2,
+  plumpness: 8
+  },
+  {
+  submitter: 'Hamid Hamidson',
+  redness: 4,
+  plumpness: 3
+  }
+  ]
+  const metric = 'redness'
+  const judgeVegetable = (vegetables, metric) => {
+  let max = 0;
+  let max2 = 0;
+  let person = '';
+  
+  vegetables.forEach(element => {
+       if (element[metric] > max) {
+           max = element[metric];
+           person = element.submitter;
+       }
+  });
+  return person;
+  }
+  console.log(judgeVegetable(vegetables, metric));
